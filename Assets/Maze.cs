37d2bdb176c8b432b7299ff162f413411524eede
GuyKroizman <kroizguy@gmail.com>
2017-10-29 00:50:26 +0300

    newer unity and generate maze from 2d c# array

--- Assets/Maze.cs
+++ Assets/Maze.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class Maze : MonoBehaviour {
 
@@ -13,50 +14,59 @@ public class Maze : MonoBehaviour {
     float maxZ;
 
 
-    void putBrick(float x, float z)
+    void PutBrick(float x, float z)
     {
+        if (x < minX)
+            return;
+        if (x > maxX)
+            return;
+        if (z < minZ)
+            return;
+        if (z > maxZ)
+            return;
+
         x += brick.transform.localScale.x / 2;
         z += brick.transform.localScale.z / 2;
         Instantiate(brick, new Vector3(x, 1, z), Quaternion.identity);
     }
 
-    void createMazeOuterWalls()
+    void CreateMazeOuterWalls()
     {
-        buildNorthWall();
-        buildWestWallSkipFirstBlockWhichIsPartOfNorthWall();
-        buildSouthWallSkipFirstBlockWhichIsPartOfWestWall();
-        buildEastWallSkipFirstAndLastBricksWhichAlreadyBuilt();
+        BuildNorthWall();
+        BuildWestWallSkipFirstBlockWhichIsPartOfNorthWall();
+        BuildSouthWallSkipFirstBlockWhichIsPartOfWestWall();
+        BuildEastWallSkipFirstAndLastBricksWhichAlreadyBuilt();
     }
 
-    private void buildEastWallSkipFirstAndLastBricksWhichAlreadyBuilt()
+    private void BuildEastWallSkipFirstAndLastBricksWhichAlreadyBuilt()
     {
         for (float i = minZ + 1; i < maxZ - 1; i++)
         {
-            putBrick(maxX - 1, i);
+            PutBrick(maxX - 1, i);
         }
     }
 
-    private void buildSouthWallSkipFirstBlockWhichIsPartOfWestWall()
+    private void BuildSouthWallSkipFirstBlockWhichIsPartOfWestWall()
     {
         for (float i = minX + 1; i < maxX; i++)
         {
-            putBrick(i, maxZ - 1);
+            PutBrick(i, maxZ - 1);
         }
     }
 
-    private void buildWestWallSkipFirstBlockWhichIsPartOfNorthWall()
+    private void BuildWestWallSkipFirstBlockWhichIsPartOfNorthWall()
     {
         for (float i = minZ + 1; i < maxZ; i++)
         {
-            putBrick(minX, i);
+            PutBrick(minX, i);
         }
     }
 
-    private void buildNorthWall()
+    private void BuildNorthWall()
     {
         for (float i = minX; i < maxX; i++)
         {
-            putBrick(i, minZ);
+            PutBrick(i, minZ);
         }
     }
 
@@ -71,11 +81,58 @@ public class Maze : MonoBehaviour {
         minZ = -1.0f * floorSize.z / 2.0f;
         maxZ = floorSize.z / 2.0f;
 
-        createMazeOuterWalls();
+        CreateMazeOuterWalls();
+
+        CreateMazeInnerLayout();
     }
-	
-	
-	void Update () {
+
+    private void CreateMazeInnerLayout()
+    {
+        int [,] layout = new int[,]
+        {
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            {0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0 },
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0 },
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0 },
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0 },
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0 },
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0 },
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0 },
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0 },
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0 },
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0 },
+            {0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0 },
+            {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 0 },
+            {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0 },
+            {0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            {0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            {0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            {0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            {0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            {0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
+        };
+
+        for(int x = (int)minX; x < maxX; x++)
+        {
+            if (x + Math.Abs(minX)> layout.GetLength(0))
+                continue;
+
+            for (int z = (int)minZ; z<maxZ; z++)
+            {
+                if (z + Math.Abs(minZ) > layout.GetLength(1))
+                    continue;
+
+                if(layout[x + (int)Math.Abs(minX), z+(int)Math.Abs(minZ)] == 1)
+                    PutBrick(x, z);
+            }
+        }
+    }
+
+    void Update () {
 	
 	}
 }
