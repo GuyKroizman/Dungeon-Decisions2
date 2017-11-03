9d9c29879dcd115f87da69360f50192a4a500a64
GuyKroizman <kroizguy@gmail.com>
2017-11-03 20:54:24 +0200

    turn player left and right by keyboard

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,122 +1,83 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour
+{
+
+  
+    
+    class TurnInfo
+    {
+        private float rotation = 0.0f;
+        private Quaternion m_RotateDestinationQuaternion = Quaternion.identity;
+
+        public void TurnRight()
+        {
+            rotation += 90.0f;
+            m_RotateDestinationQuaternion = Quaternion.Euler(0.0f, rotation, 0.0f);
+        }
+
+        public void TurnLeft()
+        {
+            rotation -= 90.0f;
+            m_RotateDestinationQuaternion = Quaternion.Euler(0.0f, rotation, 0.0f);
+        }
+
+        internal Quaternion getTurnDestination()
+        {
+            return m_RotateDestinationQuaternion;
+        }
+
+        internal float getTurningSpeed()
+        {
+            return 175;
+        }
+    }
+
+    TurnInfo m_TurnInfo;
 
     Rigidbody m_rigidbody;
 
-    void Start () {
+    void Start()
+    {
         m_rigidbody = GetComponent<Rigidbody>();
-    }
-	
-	// Update is called once per frame
-	void Update () {
 
+        m_TurnInfo = new TurnInfo();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RotateToDestination(m_TurnInfo.getTurnDestination(), m_TurnInfo.getTurningSpeed());
+    }
+
+    private void RotateToDestination(Quaternion rotateDestinationQuaternion, float turnSpeed)
+    {
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, rotateDestinationQuaternion, turnSpeed * Time.deltaTime);
     }
 
     public void move(int asdf)
     {
-        
         if (asdf == 0)
         {
             m_rigidbody.AddForce(transform.forward * 10);
         }
-            //MoveForward();
+
 
         if (asdf == 1)
         {
-            //Vector2 direction = rigidbody.velocity.normalized;
-            //Vector3.Angle(rigidbody.transform.position, direction);
-            //Vector3 cross = Vector3.Cross(rigidbody.transform.position, direction);
-            //Quaternion q = Quaternion.Euler(cross);
-            //rigidbody.MoveRotation(q);
-
-            //TurnRight();
+            m_TurnInfo.TurnLeft();                      
         }
 
         if (asdf == 2)
         {
-            //TurnLeft();
+            m_TurnInfo.TurnRight();            
         }
+
+        
     }
 
-    //private void MoveForward()
-    //{
-    //    GetComponent<Rigidbody>().AddForce(transform.forward * 3);
-    //    //if (lookingDir == DIRECTIONS.N)
-    //    //{
-    //    //    transform.position += Vector3.up;
-
-    //    //}
-    //    //else if (lookingDir == DIRECTIONS.S)
-    //    //{
-    //    //    transform.position += Vector3.down;
-
-    //    //}
-    //    //else if (lookingDir == DIRECTIONS.W)
-    //    //{
-    //    //    transform.position += Vector3.left;
-
-    //    //}
-    //    //else if (lookingDir == DIRECTIONS.E)
-    //    //{
-    //    //    transform.position += Vector3.right;
-
-    //    //}
-    //}
-
-    //private void TurnLeft()
-    //{
-    //    if (lookingDir == DIRECTIONS.N)
-    //    {
-    //        Camera.main.transform.rotation = Quaternion.Euler(0, 270, 90);
-    //        //SetDir(CharController.DIRECTIONS.W);
-    //    }
-    //    else if (lookingDir == DIRECTIONS.S)
-    //    {
-    //        Camera.main.transform.rotation = Quaternion.Euler(0, 90, 270);
-    //        //SetDir(CharController.DIRECTIONS.E);
-    //    }
-    //    else if (lookingDir == DIRECTIONS.W)
-    //    {
-    //        Camera.main.transform.rotation = Quaternion.Euler(90, 180, 0);
-    //        //SetDir(CharController.DIRECTIONS.S);
-    //    }
-    //    else if (lookingDir == DIRECTIONS.E)
-    //    {
-    //        Camera.main.transform.rotation = Quaternion.Euler(270, 0, 0);
-    //        //SetDir(CharController.DIRECTIONS.N);
-    //    }
-    //}
-
-    //private void TurnRight()
-    //{
-    //    if (lookingDir == DIRECTIONS.N)
-    //    {
-    //        Camera.main.transform.rotation = Quaternion.Euler(0, 90, 270);
-    //        //SetDir(CharController.DIRECTIONS.E);
-    //    }
-    //    else if (lookingDir == DIRECTIONS.S)
-    //    {
-    //        Camera.main.transform.rotation = Quaternion.Euler(0, 270, 90);
-    //        //SetDir(CharController.DIRECTIONS.W);
-    //    }
-    //    else if (lookingDir == DIRECTIONS.W)
-    //    {
-    //        Camera.main.transform.rotation = Quaternion.Euler(270, 0, 0);
-    //        //SetDir(CharController.DIRECTIONS.N);
-    //    }
-    //    else if (lookingDir == DIRECTIONS.E)
-    //    {
-    //        Camera.main.transform.rotation = Quaternion.Euler(90, 180, 0);
-    //        //SetDir(CharController.DIRECTIONS.S);
-    //    }
-    //}
-
-    //public void SetDir(DIRECTIONS dir)
-    //{
-    //    lookingDir = dir;
-    //}
 
 }
