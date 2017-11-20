07aaa7ea34298e7c32c151322063df28b06549c5
GuyKroizman <kroizguy@gmail.com>
2017-11-21 00:24:04 +0200

    random color for bricks

---
+++ Assets/Brick.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brick : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Renderer rend = GetComponent<Renderer>();
+
+        rend.material.color = Random.ColorHSV();
+    }
+
+
+}
